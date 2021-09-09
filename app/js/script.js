0ed118e868a604e2699d1c5f59ae628d80e383ea
Yaroslaw <fitelyaroslaw@gmail.com>
2021-09-09 12:57:29 +0300

    responsive section

--- app/js/script.js
+++ app/js/script.js
@@ -1,16 +1,24 @@
 let btnHamburger = document.querySelector("#btnHamburger");
+let body = document.querySelector("body");
 let header = document.querySelector(".header");
 let overlay = document.querySelector(".overlay");
+let fadeElems = document.querySelectorAll(".has-fade");
 
 btnHamburger.addEventListener("click", () => {
 
     if(header.classList.contains("open")) {
+        body.classList.remove("noscroll");
         header.classList.remove("open");
-        overlay.classList.remove("fade-in");
-        overlay.classList.add("fade-out");
+        fadeElems.forEach( (element) => {
+            element.classList.remove("fade-in");
+            element.classList.add("fade-out");
+        });
     } else {
+        body.classList.add("noscroll");
         header.classList.add("open");
-        overlay.classList.remove("fade-out");
-        overlay.classList.add("fade-in");
+        fadeElems.forEach( (element) => {
+            element.classList.remove("fade-out");
+            element.classList.add("fade-in");
+        });
     };
 });
